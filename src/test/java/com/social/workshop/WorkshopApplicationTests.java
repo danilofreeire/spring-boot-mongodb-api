bdec3f2f16612da0c25832e91f88a4f3a595e2f4
Danilo Freire <danilofreirea90@gmail.com>
2024-06-20 16:41:40 -0300

    Primeiro commit

---
+++ src/test/java/com/social/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
